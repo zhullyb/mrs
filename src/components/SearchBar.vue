12145f26fb65344b7a4ef11c7e4f4ac73c7327bc
zhullyb <zhullyb@outlook.com>
2023-12-12 18:35:18 +0800

    components: add SearchBar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const searchContent = ref<String>('')
+const search = () => {
+    console.log(searchContent.value)
+}
+
+</script>
+
+<template>
+    <div>
+        <a-input-search
+            v-model:value="searchContent"
+            placeholder="搜索电影"
+            enter-button
+            @search="search"
+        />
+    </div>
+</template>
