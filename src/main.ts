a0d95d7de85da49ed1d51975792d78e621b2ea81
zhullyb <zhullyb@outlook.com>
2023-12-12 16:54:34 +0800

    import pinia

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
+import pinia from './stores/'
 import Antd from 'ant-design-vue'
 import App from './App.vue'
 import 'ant-design-vue/dist/reset.css'
 
 const app = createApp(App)
 
+app.use(pinia)
 app.use(Antd)
 
 app.mount('#app')
