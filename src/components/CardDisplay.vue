8290776d9e4e448f8b7edd4182f6371c5ad0fe3c
zhullyb <zhullyb@outlook.com>
2023-12-14 19:54:48 +0800

    CardDisplay.vue: display the entrance for admin

--- src/components/CardDisplay.vue
+++ src/components/CardDisplay.vue
@@ -3,10 +3,18 @@ import { onMounted, ref } from 'vue';
 import movieService from '../apis/movieService';
 import { movieListItem } from '../types/movie';
 import { useRouter } from 'vue-router';
+import userStore from '../stores/userStore';
 const router = useRouter();
+const newUserStore = userStore();
 const openUrl = (mid: string) => {
     router.push({
         path: `/movie/${mid}`,
+    });
+};
+
+const addMovie = () => {
+    router.push({
+        path: '/addMovie',
     });
 };
 const data = ref<Array<movieListItem>>()
@@ -19,6 +27,19 @@ onMounted(async () => {
 
 <template>
     <a-row class="root">
+        <a-col :span="6">
+            <a-card v-show="newUserStore.userSession.level == 1" class="card" hoverable @click="addMovie">
+                <template #cover>
+                    <img src="/addMovie.jpg" alt="添加电影" class="cover-image">
+                </template>
+                <a-card-meta title="添加电影">
+                    <template #description>
+                        <a-rate value="5" disabled class="rate-class" />
+                        <span class="rate-num"></span>
+                    </template>
+                </a-card-meta>
+            </a-card>
+        </a-col>
         <a-col :span="6" v-for="item in data">
             <a-card hoverable class="card" @click="openUrl(item.mid)">
                 <template #cover>
